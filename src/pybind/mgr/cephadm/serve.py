9d12758c648dc69e2d544ff6d1eabd43d26b11fa
Joshua Blanch <joshua.blanch@clyso.com>
2025-03-19 00:45:30 +0000

    mgr/cephadm: view, cancel and force existing scheduled actions
    Signed-off-by: Joshua Blanch <joshua.blanch@clyso.com>

--- src/pybind/mgr/cephadm/serve.py
+++ src/pybind/mgr/cephadm/serve.py
@@ -1106,7 +1106,6 @@ class CephadmServe:
 
             # ignore unmanaged services
             if spec and spec.unmanaged:
-                # remove any pending scheduled actions in the event users switch back from unmanged to managed
                 continue
 
             # ignore daemons for deleted services
@@ -1138,7 +1137,7 @@ class CephadmServe:
                 last_deps = []
 
             action = self.mgr.cache.get_scheduled_daemon_action(dd.hostname, dd.name())
-            
+
             if not last_config:
                 self.log.info('Reconfiguring %s (unknown last config time)...' % (
                     dd.name()))
@@ -1187,7 +1186,7 @@ class CephadmServe:
                     action = 'redeploy'
 
                 is_force = self.mgr.cache.is_force_action(dd.hostname, dd.name())
-                if not is_force and action in ['restart','stop','redeploy']:
+                if not is_force and action in ['restart', 'stop', 'redeploy']:
                     r = service_registry.get_service(daemon_type_to_service(
                         dd.daemon_type)).ok_to_stop([dd.daemon_id])
                     if r.retval:
@@ -1197,6 +1196,7 @@ class CephadmServe:
                     daemon_spec = CephadmDaemonDeploySpec.from_daemon_description(dd)
                     self.mgr._daemon_action(daemon_spec, action=action)
                     if self.mgr.cache.rm_scheduled_daemon_action(dd.hostname, dd.name()):
+                        self.mgr.cache.clear_force_action(dd.hostname, dd.name())
                         self.mgr.cache.save_host(dd.hostname)
                 except OrchestratorError as e:
                     self.log.exception(e)
